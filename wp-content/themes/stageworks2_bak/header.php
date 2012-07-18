0ba202415c3c19514d8f9d74e9fa24b2d854703b
EMMETT PICKEREL <emmett@EMMETTs-MacBook-Air.local>
2012-07-18 12:36:34 -0700

    Ready to push to github

--- wp-content/themes/stageworks2_bak/header.php
+++ wp-content/themes/stageworks2_bak/header.php
@@ -72,7 +72,8 @@
 	<header id="branding" role="banner">
 			<hgroup>
 			  <div id="titlearea">
-          <h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?><span class="chrome1"></span></a></span></h1>
+          <span class="chrome1"></span>
+          <h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
           <h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
         </div>
 			</hgroup>
